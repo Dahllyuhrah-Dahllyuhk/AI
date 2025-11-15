945d949c174335be723dfdfc1ec268de2cacfd12
EverJun2 <deverjun2@gmail.com>
2025-11-15 15:15:57 +0900

    feat : 사용자 질의 카테고리 분류 기능 추가

--- app/main.py
+++ app/main.py
@@ -1,31 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from app.gemini import generate_text
 from pydantic import BaseModel
 import uvicorn
-
+import json
 # cd C:\Users\HJ\Desktop\AI
 # uvicorn app.main:app --reload
 
 app = FastAPI(title="AI Server", version="1.0.0")
 
-# 요청 모델 정의
 class RequestBody(BaseModel):
     prompt: str
 
-# 텍스트 생성 엔드포인트
-@app.post("/generate")
-def generate(request: RequestBody):
+# 카테고리 분류
+def classify(request: RequestBody):
     try:
-        result = generate_text(request.prompt, model="gemini-2.5-flash")
+        prompt = (
+            "다음 요청문을 일정생성/일정조회/일정삭제/일정수정/모임생성 중 하나로 분류하세요. "
+            "해당하지 않으면 'null' 을 반환하세요.\n"
+            f"요청문: {request.prompt}"
+        )
+
+        result = generate_text(prompt, model="gemini-2.5-flash")
+
         return {"result": result}
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# 기본 루트 확인용
-@app.get("/")
-def root():
-    return {"message": "서버 동작중!"}
 
-# 로컬 실행 명령
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000, reload=False)
